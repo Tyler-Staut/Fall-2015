3ed20e5da57a7672c172a4f3933b5e963535bb7d
SenorSanta <rocky735@bellsouth.net>
2015-09-09 14:29:22 -0400

    Fixed SAS and SSS

--- GitHub-Python/Triangle/Triangle.py
+++ GitHub-Python/Triangle/Triangle.py
@@ -8,7 +8,11 @@ import math
 print("Find all the info of your triangle!") 
 print("You may choose from SAS, SSS, ASA, or AAS theorems") #Support for the ambiguous case needed
 #--------------------Definitions of Theorems--------------------#
-def SAS(SideA, AngleB, SideC): #Needs to use Law of Sines and Law of Cosines to be correct.
+def SAS(SideA, AngleB, SideC): #Assume Angle C is the largest, and Angle A is smallest.
+    SideB = math.sqrt(SideA**2 + SideC**2 - 2*SideA*SideC*math.cos(AngleB))
+    AngleA = math.asin(SideA*math.sin(AngleB)/SideB)
+    AngleC = math.pi - (AngleA + AngleB)
+    
     
 
 def SSS(SideA, SideB, SideC): #Needs to use Law of Cosines to find 3 angle values.
