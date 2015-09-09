51d73347d5c1c2496018aa2236d1e643b8caeb26
SenorSanta <rocky735@bellsouth.net>
2015-09-09 09:45:59 -0400

    Added things

--- GitHub-Python/Triangle/Triangle.py
+++ GitHub-Python/Triangle/Triangle.py
@@ -8,5 +8,15 @@ import math
 print("Find all the info of your triangle!") 
 print("All angles must be in degrees.")
 print("You may choose SAS, SSS, ASA, or AAS")
+
+def SAS(Side1, Angle1, Side2):
+    areaSAS = (Side1*Side2*math.sin(Angle1))/2
+    print(int(areaSAS))
+    Angle2 = math.asin((Side1/Side2))
+    Angle3 = math.pi - (Angle1 + Angle2)
+    Side3 = Side2*math.sin(Angle1)
+
+def SSS(Side1, Side2, Side3):
+    areaSSS = 
 input("Which theorem would you like to use? ")
 
