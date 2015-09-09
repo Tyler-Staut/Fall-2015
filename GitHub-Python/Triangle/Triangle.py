b8fecd0cf1f7d790c1d4e6ba1003887fd74132d6
SenorSanta <rocky735@bellsouth.net>
2015-09-09 10:45:36 -0400

    Determined Math Errors to be Fixed

--- GitHub-Python/Triangle/Triangle.py
+++ GitHub-Python/Triangle/Triangle.py
@@ -6,19 +6,18 @@ Created on Sep 9, 2015
 import math
 
 print("Find all the info of your triangle!") 
-print("All angles must be in degrees.")
-print("You may choose SAS, SSS, ASA, or AAS")
+print("You may choose from SAS, SSS, ASA, or AAS theorems") #Support for the ambiguous case needed
+#--------------------Definitions of Theorems--------------------#
+def SAS(Side1, Angle1, Side2): #Needs to use Law of Sines and Law of Cosines to be correct.
+    
 
-def SAS(Side1, Angle1, Side2):
-    areaSAS = (Side1*Side2*math.sin(Angle1))/2
-    print(int(areaSAS))
-    Angle2 = math.asin((Side1/Side2))
-    Angle3 = math.pi - (Angle1 + Angle2)
-    Side3 = Side2*math.sin(Angle1)
-
-def SSS(Side1, Side2, Side3):
+def SSS(Side1, Side2, Side3): #Needs to use Law of Cosines to find 3 angle values.
     s = (Side1 + Side2 + Side3)/2
     areaSSS = math.sqrt(s*(s - Side1)*(s - Side2)*(s - Side3))
-    Angle1 = 
+    
+    
+def ASA(Angle1, Side1, Angle2):
+    
+#----------------------------------------------------------------#
 input("Which theorem would you like to use? ")
 
