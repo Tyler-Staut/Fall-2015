8078e282af1558b8d82a709c38537a0019d12909
SenorSanta <rocky735@bellsouth.net>
2015-09-09 09:21:43 -0400

    Moved triangle project into shared.

---
+++ GitHub-Python/Triangle/Triangle.py
@@ -0,0 +1,12 @@
+'''
+Created on Sep 9, 2015
+
+@author: Matt
+'''
+import math
+
+print("Find all the info of your triangle!") 
+print("All angles must be in degrees.")
+print("You may choose SAS, SSS, ASA, or AAS")
+input("Which theorem would you like to use? ")
+
