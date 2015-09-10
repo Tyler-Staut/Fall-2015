7b035e31d9f9f546bf446adbb996458a1b14b16b
SenorSanta <rocky735@bellsouth.net>
2015-09-09 22:41:09 -0400

    Finished except for Ambiguous Case

--- GitHub-Python/Triangle/Triangle.py
+++ GitHub-Python/Triangle/Triangle.py
@@ -11,75 +11,73 @@ print("You may choose from SAS, SSS, ASA, or AAS theorems") #Support for the amb
 #Angles will eventually be converted into degrees in code for ease of use.
 
 
-triangleTheorem = str(input("Which theorem would you like to use? "))
-
 #--------------------Definitions of Theorems--------------------#
 def SAS(SideA, AngleB, SideC): #Assume Angle C is the largest, and Angle A is smallest.
     areaSAS = float((SideA*SideC*math.sin(AngleB))/2)
     SideB = float(math.sqrt(SideA**2 + SideC**2 - 2*SideA*SideC*math.cos(AngleB)))
-    AngleA = float(math.asin(SideA*math.sin(AngleB)/SideB))
-    AngleC = float(math.pi - (AngleA + AngleB))
+    AngleA = float(math.asin(SideA*math.sin(AngleB)/SideB)*(180/math.pi))
+    AngleC = float(math.asin(SideC*math.sin(AngleB)/SideB)*(180/math.pi))
     print("Side B = ", SideB)
     print("Angle A = ", AngleA)
     print("Angle C = ", AngleC)
     print("Area = ", areaSAS)
 
-#SSS currently unavailable due to prominent domain errors
-'''def SSS(SideA, SideB, SideC): 
+def SSS(SideA, SideB, SideC): 
     s = (SideA + SideB + SideC)/2
-    areaSSS = math.sqrt(s*(s - SideA)*(s - SideB)*(s - SideC))
-    AngleA = math.acos((SideB**2 - SideA**2 + SideC**2)/2*SideB*SideC)
-    AngleB = math.acos((SideA**2 - SideB**2 + SideC**2)/2*SideA*SideC)
-    AngleC = math.acos((SideA**2 + SideB**2 - SideC**2)/2*SideA*SideB)
+    areaSSS = float(math.sqrt(s*(s - SideA)*(s - SideB)*(s - SideC)))
+    AngleA = float(math.acos((SideB**2 - SideA**2 + SideC**2)/(2*SideB*SideC))*(180/math.pi))
+    AngleB = float(math.acos((SideA**2 - SideB**2 + SideC**2)/(2*SideA*SideC))*(180/math.pi))
+    AngleC = float(math.acos((SideA**2 + SideB**2 - SideC**2)/(2*SideA*SideB))*(180/math.pi))
     print("Angle A = ", AngleA)
     print("Angle B = ", AngleB)
     print("Angle C = ", AngleC)
-    print("Area = ", areaSSS)       '''
+    print("Area = ", areaSSS)
     
 def ASA(AngleA, SideC, AngleB):
-    AngleC = math.pi - (AngleA + AngleB)
-    SideA = (SideC*math.sin(AngleA)/math.sin(AngleC))
-    SideB = (SideC*math.sin(AngleB)/math.sin(AngleC))
-    areaASA = (SideC**2*math.sin(AngleA)*math.sin(AngleB)/(2*math.sin(AngleC)))
+    AngleC = float(math.pi - (AngleA + AngleB))*(180/math.pi)
+    SideA = float((SideC*math.sin(AngleA)/math.sin((AngleC*(math.pi/180)))))
+    SideB = float((SideC*math.sin(AngleB)/math.sin((AngleC*(math.pi/180)))))
+    areaASA = float((SideC**2*math.sin(AngleA)*math.sin(AngleB)/(2*math.sin((AngleC*(math.pi/180))))))
     print("Angle C = ", AngleC)
     print("Side A = ", SideA)
     print("Side B = ", SideB)
     print("Area = ", areaASA)
 
-def AAS(AngleA, AngleB, SideC): 
-    AngleC = math.pi - (AngleA + AngleB)
-    SideA = (SideC*math.sin(AngleA)/math.sin(AngleC))
-    SideB = (SideC*math.sin(AngleB)/math.sin(AngleC))
-    areaAAS = (SideC**2*math.sin(AngleA)*math.sin(AngleB)/(2*math.sin(AngleC)))
+#AAS function under construction at the moment
+def AAS(AngleA, AngleB, SideA): 
+    AngleC = float(math.pi - (AngleA + AngleB))*(180/math.pi)
+    SideC = float(SideA/(math.cos(AngleB)))
+    SideB = float(SideA*math.tan(AngleB))
+    areaAAS = float((SideC**2*math.sin(AngleA)*math.sin(AngleB)/(2*math.sin((AngleC*(math.pi/180))))))
     print("Angle C = ", AngleC)
-    print("Side A = ", SideA)
+    print("Side C = ", SideC)
     print("Side B = ", SideB)
     print("Area = ", areaAAS)
 #----------------------------------------------------------------#
+triangleTheorem = str(input("Which theorem would you like to use?"))
 if triangleTheorem in ('SAS', 'SSS', 'ASA', 'AAS'):
         if triangleTheorem == 'SAS':
             SideA = float(input("Side A: "))
-            AngleB = float(input("Angle B (in radians): "))
+            AngleB = float(input("Angle B (in degrees): "))*(math.pi/180)
             SideC = float(input("Side C: "))
             SAS(SideA, AngleB, SideC)
         elif triangleTheorem == 'SSS':
-            print("All sides must meet in a triangle.")
+            print("The three sides must meet absolutely perfectly in order to work.")
             SideA = float(input("Side A: "))
             SideB = float(input("Side B: "))
+            print("Length of Side C must be between ", float(math.sqrt(SideA**2 + SideB**2 - 2*SideA*SideB)), "and", float(math.sqrt(SideA**2 + SideB**2 + 2*SideA*SideB)))
             SideC = float(input("Side C: "))
             SSS(SideA, SideB, SideC)
         elif triangleTheorem == 'ASA':
-            AngleA = float(input("Angle A (in radians): "))
+            AngleA = float(input("Angle A (in degrees): "))*(math.pi/180)
             SideC = float(input("Side C: "))
-            AngleB = float(input("Angle B (in radians): "))
+            AngleB = float(input("Angle B (in degrees): "))*(math.pi/180)
             ASA(AngleA, SideC, AngleB)
         elif triangleTheorem == 'AAS':
-            AngleC = float(input("Angle C (in radians):"))
+            AngleA = float(input("Angle A (in degrees):"))*(math.pi/180)
+            AngleB = float(input("Angle B (in degrees):"))*(math.pi/180)
             SideA = float(input("Side A: "))
-            SideB = float(input("Side B: "))
-            AAS(AngleA, AngleB, SideC)
-        
-    
+            AAS(AngleA, AngleB, SideA)    
 else:
     print("Input undefined; Try again.")
     
