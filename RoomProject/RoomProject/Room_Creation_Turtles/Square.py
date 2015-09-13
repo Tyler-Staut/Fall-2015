7b5ade68c12f1bd65169023c64769fe29e361a8b
SenorSanta <rocky735@bellsouth.net>
2015-09-13 16:47:49 -0400

    Just because I can.

--- RoomProject/RoomProject/Room_Creation_Turtles/Square.py
+++ RoomProject/RoomProject/Room_Creation_Turtles/Square.py
@@ -7,6 +7,15 @@ Created on Sep 5, 2015
 
 import turtle
 
+length = float(input("What is the length of the square? "))
+width = float(input("What is the width of the square? "))
 
-michaelBeans = turtle.Turtle()
+turtle.forward(width)
+turtle.left(90)
+turtle.forward(length)
+turtle.left(90)
+turtle.forward(width)
+turtle.left(90)
+turtle.forward(length)
+turtle.mainloop()
 
