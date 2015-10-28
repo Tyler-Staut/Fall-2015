706f452b7b7d48f2c3b8ede6655fdf09f66c6b4c
Tyler <trsman44@GMAIL.COM>
2015-10-28 11:07:37 -0400

    Fix

--- CPSC PROJECT 1/MainProgramFirstProject.py
+++ CPSC PROJECT 1/MainProgramFirstProject.py
@@ -55,15 +55,15 @@ class ComputerScienceOne_MWF_10AM():
             print("{:<15} {:<15} {}".format(fields[1].strip(), fields[2].strip(), fields[0].strip()))
         myfile.close()
         
-        
+    #------------------------------------------------------------------------------#
         
     def Top10WithMostPeople(self):
         pass
     
-    def PercentageOldPweps(self):
+    def PercentageOldGeezers(self):
         pass
     
-    def MaleToFemaleXLR(self):
+    def MaleToFemaleXLRcable(self):
         pass
     
     def NoChildren(self):
