4a434092518d2bfadc49c8253581b32eeb8750df
SenorSanta <rocky735@bellsouth.net>
2015-11-04 15:59:23 -0500

    FIXED THIS FOR THE LAST DAMN TIME... ON TO THE NEXT ONE, AMERICA

--- CPSC PROJECT 1/MainProgramFirstProjectMATT.py
+++ CPSC PROJECT 1/MainProgramFirstProjectMATT.py
@@ -163,7 +163,7 @@ class MainFunctions():
         for country in range(len(T.ListOfCountries)):
             count += 1
             if T.ListOfCountries[country][0] == Letter:
-                print(T.SortedAll[country])
+                print("  {:<28}      {:<15} {:<10}  ".format(T.SortedAll[country][0], T.SortedAll[country][1], T.SortedAll[country][2],))
                      
                      
     def Option4(self):  #Top 10 Countries with the most people
